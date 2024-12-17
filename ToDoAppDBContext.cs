de2551e2ad833094672be0165502bfb544808b39
oykenn <152690792+oykenn@users.noreply.github.com>
2024-12-17 08:51:02 -0500

    Add files via upload

---
+++ ToDoAppDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryApp.models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace LibraryApp.db
+{
+    internal class ToDoAppDBContext : DbContext
+    {
+        public DbSet<models.MyTodo> MyTodos { get; set; }
+      
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=todo-db;");
+        }
+    }
+}
